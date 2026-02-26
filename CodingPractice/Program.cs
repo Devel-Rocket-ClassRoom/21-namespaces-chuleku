0cbcd91d3cfa33fc8bb9828e051689dc71f2a8f6
chuleku <skatkdwns08@gmail.com>
2026-02-26 11:38:52 +0900

    C# NameSpace

--- CodingPractice/Program.cs
+++ CodingPractice/Program.cs
@@ -1,4 +1,61 @@
 using System;
+using static System.Console;
+using static System.Math;
 
-// README.md를 읽고 아래에 코드를 작성하세요.
-Console.WriteLine("코드를 작성하세요.");
+///과제 1: 정규화된 이름으로 네임스페이스 사용
+/*Foo.Car car = new Foo.Car();
+car.mesage();
+
+Bar.Car bar = new Bar.Car();
+bar.mesage();
+namespace Foo
+{
+    public class Car
+    {
+        public void mesage()
+        {
+            Console.WriteLine("Foo 네임스페이스의 Car 클래스");
+        }
+    }
+}
+namespace Bar
+{
+    public class Car
+    {
+        public void mesage()
+        {
+            Console.WriteLine("Bar 네임스페이스의 Car 클래스");
+        }
+    }
+}*/
+///과제 2: 중첩 네임스페이스 활용
+/*Korea.Seoul.Car seoulcar = new Korea.Seoul.Car();
+seoulcar.run();
+Korea.Incheon.Car Incheoncar = new Korea.Incheon.Car();
+Incheoncar.run();
+namespace Korea.Seoul
+{
+    public class Car
+    {
+        public void run()
+        {
+            Console.WriteLine("서울 자동차가 달립니다.");
+        }
+    }
+}
+namespace Korea.Incheon
+{
+    public class Car
+    {
+        public void run()
+        {
+            Console.WriteLine("인천 자동차가 달립니다.");
+        }
+    }
+}*/
+///과제 3: using static 지시문
+WriteLine("Hello, World!");
+double result = Sqrt(16);
+WriteLine($"제곱근: {result}");
+double Maxvalue = Max(10, 20);
+WriteLine($"최댓값: {Maxvalue}");
--- GameSystem/Program.cs
+++ GameSystem/Program.cs
@@ -1,4 +1,56 @@
 using System;
+MyGame.Characters.Player hero = new MyGame.Characters.Player();
+MyGame.Characters.Enemy Goblin = new MyGame.Characters.Enemy();
+Console.WriteLine("=== 캐릭터 시스템 ===");
+hero.Attack();
+Goblin.TakeDamage();
+Console.WriteLine();
+MyGame.Items.Weapon FireSword = new MyGame.Items.Weapon();
+MyGame.Items.Potion HealingPotion = new MyGame.Items.Potion();
+Console.WriteLine("=== 아이템 시스템 ===");
+FireSword.GetDamage(50);
+HealingPotion.Use();
 
-// README.md를 읽고 아래에 코드를 작성하세요.
-Console.WriteLine("코드를 작성하세요.");
+
+
+
+namespace MyGame.Characters
+{
+    class Player
+    {
+        public string Name = "용사";
+
+        public void Attack()
+        {
+            Console.WriteLine($"플레이어 {Name}(이)가 공격합니다!");
+        }
+    }
+    class Enemy
+    {
+        public string Type = "고블린";
+        public void TakeDamage()
+        {
+            Console.WriteLine($"적 {Type}(이)가 데미지를 받았습니다!");
+        }
+    }
+}
+namespace MyGame.Items
+{
+    class Weapon
+    {
+        public string Name = "불꽃 검";
+        public void GetDamage(int damage)
+        {
+            Console.WriteLine($"무기 {Name}의 공격력: {damage}");
+        }
+    }
+    class Potion
+    {
+        public string Name = "체력 포션";
+
+        public void Use()
+        {
+            Console.WriteLine($"포션 {Name}을(를) 사용했습니다.");
+        }
+    }
+}
